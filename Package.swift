12329be1cb25f6a6c1a547b6ba9ab87a9b775840
Kodex <205018495+KODEX-DEV-GIT@users.noreply.github.com>
2026-02-19 11:49:57 +0500

    adjusted view for spm

--- Package.swift
+++ Package.swift
@@ -23,5 +23,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "Viewer"
+    name: "Viewer",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Viewer",
+            targets: ["Viewer"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Viewer"
+        ),
+    ]
 )
